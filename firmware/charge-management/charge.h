2611751beba99f1419d55dc7f1f9016e52cc5644
FrancescoBrunelli <brunellifrancesco21@gmail.com>
2026-06-11 12:01:47 +0200

    feat: Initial commit of charge management system files

---
+++ firmware/charge-management/charge.h
@@ -0,0 +1,28 @@
+#ifndef __CHARGEMANAGEMENT__
+#define __CHARGEMANAGEMENT__
+
+float Voltage();
+
+float Current();
+
+float State_of_charge();
+
+bool check_for_connection();
+
+void handshake();
+
+void readSensors();
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
