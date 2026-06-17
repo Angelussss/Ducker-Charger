1312c958928e3b12906885d66a28e4ef2558ee89
FrancescoBrunelli <brunellifrancesco21@gmail.com>
2026-06-17 17:18:28 +0200

    fix: fixed adc sensor reading and added get functions for easy data fetch

--- firmware/charge-management/charge.h
+++ firmware/charge-management/charge.h
@@ -6,6 +6,7 @@
 // Sensors:
 typedef struct {
     float tempZone[4];
+    float onBoardTemperature;
     float usbCInputCurrent;
     float power_sys_W;
 } SensorData;
@@ -16,7 +17,7 @@ typedef struct {
     bool OTD;           // (BIT6) Overtemperature in Discharge condition is detected. True when set
     bool BATHI;         // (BIT5) High battery voltage condition
     bool BATLOW;        // (BIT4) Low battery voltage condition
-    bool CHG_INH;       // (BIT3) Unable to begin chargin
+    bool CHG_INH;       // (BIT3) Unable to start charging
     bool XCHG;          // (BIT2) Charging not allowed
     bool FC;            // (BIT1) Full Charge detected
     bool CHG;           // (BIT0) (Fast) charging allowed. True when set
@@ -43,6 +44,24 @@ typedef struct {
     fuelGaugeFlags flags;
 } FuelGaugeSensors;
 
+typedef struct {
+    bool isSink;
+    bool isPlugged;
+    bool isNegotiationDone;
+    float voltage;
+    float maxCurrent;
+    float operatingCurrent;
+} PDContract;
+
+typedef struct {
+    bool overVoltageProtection;
+    bool shortCircuitProtection;
+    bool powerOn;
+    bool overTemperatureProtection;
+    bool overTemperatureWarning;
+    bool inductorPeakCurrentProtection;
+} STPD01_Status;
+
 typedef enum {      // Possible reading states for I2C
     READ_INTERNAL_TEMPERATURE,
     READ_EXTERNAL_TEMPERATURE,
@@ -58,6 +77,16 @@ typedef enum {      // Possible reading states for I2C
     READ_STATE_OF_HEALTH,
     READ_FLAGS
 } I2C_ReadState;
+
+/*
+typedef enum {
+    FAULT_NONE = 0,
+    FAULT_OVERTEMPERATURE,
+    FAULT_BATTERY_HIGH,
+    FAULT_BATTERY_LOW,
+
+} FaultCode;
+*/
 
 // Fuel Gauge Mapping
 #define FUEL_GAUGE_ADDR ((uint16_t)(0x55 << 1))     // Convert 7-bit address to 8-bit and then cast to 16-bit
@@ -86,30 +115,12 @@ typedef enum {      // Possible reading states for I2C
 
 // STPD01 Mapping
 // (Power supply for secondary USB-C)
-#define STPD01_PD_ADDR ((uint16_t)(0x54 << 1))      // To be verified (according to GitHub repo's)
+#define STPD01_PD_ADDR ((uint16_t)(0x54 << 1))      // To be verified (according to GitHub repo)
 #define VOUT_REG_ADDR 0x00
 #define ILIM_REG_ADDR 0x01
 #define INT_STAT_REG_ADDR 0x02
-//#define SERVICES_REG_ADDR 0x05        // Contains the discharge toggle ON/OFF (default if ON)
+//#define SERVICES_REG_ADDR 0x05        // Contains the discharge toggle ON/OFF (default is ON)
 #define DIGITAL_ENABLE_REG_ADDR 0x06
-
-typedef struct {
-    bool isSink;
-    bool isPlugged;
-    bool isNegotiationDone;
-    float voltage;
-    float maxCurrent;
-    float operatingCurrent;
-} PDContract;
-
-typedef struct {
-    bool overVoltageProtection;
-    bool shortCircuitProtection;
-    bool powerOn;
-    bool overTemperatureProtection;
-    bool overTemperatureWarning;
-    bool inductorPeakCurrentProtection;
-} STPD01_Status;
 
 // Critical Signals Mapping
 #define USB_IRQ_CTRL_Pin GPIO_PIN_14        // IRQ Pin is the number 14
@@ -147,9 +158,17 @@ void primaryUSBC_ConnectionINT();
 
 void secondaryUSBC_ConnectionINT();
 
-void setupSTPD01(float voltage, float current);
+bool setupSTPD01(float voltage, float current);
 
 bool checkSTPD01();
+
+void enable_USBA1();
+
+void disable_USBA1();
+
+void enable_USBA2();
+
+void disable_USBA2();
 
 void enable_STPD01 ();
 
@@ -168,5 +187,26 @@ void selectPDO3();
 void selectPDO4();
 
 void selectPDO5();
+
+// Get data functions
+SensorData getSensorData();
+
+FuelGaugeSensors getFuelGaugeData();
+
+STPD01_Status getSTPD01_Status();
+
+PDContract getPrimaryUSBC_Contract();
+
+PDContract getSecondaryUSBC_Contract();
+
+int get_USBA1_Status();
+
+int get_USBA2_Status();
+
+int get_USBC2_Status();
+
+int get_OTG_Status();
+
+int get_STPD01_Enabled();
 
 #endif
